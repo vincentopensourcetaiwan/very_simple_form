4c3aff416ae5eb93495c47470b70f36f21eda775
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-25 16:08:09 +0800

    setup submit event

--- lib/very_simple_form_web/live/home.ex
+++ lib/very_simple_form_web/live/home.ex
@@ -4,7 +4,7 @@ defmodule VerySimpleFormWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Very Simple Form!</h1>
-    <.simple_form for={@form}>
+    <.simple_form for={@form} phx-submit="submit">
       <.input field={@form[:name]} label="Name" />
       <.button>Submit</.button>
     </.simple_form>
@@ -15,5 +15,10 @@ defmodule VerySimpleFormWeb.Home do
     form = to_form(%{"name" => ""})
     socket = assign(socket, form: form)
     {:ok, socket}
+  end
+
+  def handle_event("submit", %{"name" => name}, socket) do
+    IO.inspect(name)
+    {:noreply, socket}
   end
 end
