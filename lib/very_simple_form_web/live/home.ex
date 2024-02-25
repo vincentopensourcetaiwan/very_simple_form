1a087540721d601b7a3b4eaf0ceb11fe7f78a1c6
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-25 16:05:01 +0800

    setup simple form

--- lib/very_simple_form_web/live/home.ex
+++ lib/very_simple_form_web/live/home.ex
@@ -4,6 +4,16 @@ defmodule VerySimpleFormWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Very Simple Form!</h1>
+    <.simple_form for={@form}>
+      <.input field={@form[:name]} label="Name" />
+      <.button>Submit</.button>
+    </.simple_form>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    form = to_form(%{"name" => ""})
+    socket = assign(socket, form: form)
+    {:ok, socket}
   end
 end
